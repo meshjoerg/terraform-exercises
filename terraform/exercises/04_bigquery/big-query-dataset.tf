ff260cd037e854c44e6a741043a7c6a7ba302007
Jörg Gottschlich <jgottschlich@meshcloud.io>
2026-03-13 00:46:09 +0100

    Simplified big query template for easier access

--- terraform/exercises/04_bigquery/big-query-dataset.tf
+++ terraform/exercises/04_bigquery/big-query-dataset.tf
@@ -12,24 +12,6 @@ provider "google" {
   region  = "us-west1"
 }
 
-resource "google_storage_bucket" "example" {
-  name     = "<my-bucket-unique-id>"
-  location = "us-west1"
-
-  versioning {
-    enabled = true
-  }
-
-  labels = {
-    environment = "student-lab"
-    managed_by  = "terraform"
-  }
-}
-
-output "bucket_name" {
-  value = google_storage_bucket.example.name
-}
-
 resource "google_bigquery_dataset" "analytics_lab" {
   dataset_id                  = "analytics_lab"
   location                    = "EU"
